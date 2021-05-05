e5f180846f576e48c8411a62ae616ae37d6432bf
leonardosigrist <leonardo.sigrist@gmail.com>
2021-05-04 23:48:38 -0300

    Implementando as regras de validação

--- src/Password.Services/DefaultPasswordValidator.cs
+++ src/Password.Services/DefaultPasswordValidator.cs
@@ -1,9 +1,57 @@
-﻿namespace Password.Services
+﻿using System.Text.RegularExpressions;
+
+namespace Password.Services
 {
     public class DefaultPasswordValidator : IPasswordValidator
     {
         public bool IsValid(string password)
         {
+            return HasAtLeastNineCharacters(password)
+                && HasAtLeastOneDigit(password)
+                && HasAtLeastOneLowerCaseCharacter(password)
+                && HasAtLeastOneUpperCaseCharacter(password)
+                && HasAtLeastOneSpecialCharacter(password)
+                && HasNoRepeatedCharacter(password);
+        }
+
+        private bool HasAtLeastNineCharacters(string password)
+        {
+            return !string.IsNullOrEmpty(password) && password.Length >= 9;
+        }
+
+        private bool HasAtLeastOneDigit(string password)
+        {
+            return Regex.IsMatch(password, "[0-9]");
+        }
+
+        private bool HasAtLeastOneLowerCaseCharacter(string password)
+        {
+            return Regex.IsMatch(password, "[a-z]");
+        }
+
+        private bool HasAtLeastOneUpperCaseCharacter(string password)
+        {
+            return Regex.IsMatch(password, "[A-Z]");
+        }
+
+        private bool HasAtLeastOneSpecialCharacter(string password)
+        {
+            return Regex.IsMatch(password, @"[!@#\$%\^&\*\(\)-\+]");
+        }
+
+        private bool HasNoRepeatedCharacter(string password)
+        {
+            string incrementalPassword = "";
+            foreach (char character in password) 
+            {
+                if (incrementalPassword.IndexOf(character) >= 0)
+                {
+                    return false;
+                }
+
+                incrementalPassword += character;
+            }
+
             return true;
         }
     }
